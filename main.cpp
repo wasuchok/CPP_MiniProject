f769bd3a4be2c2f6dfa2b2d7dd613476b67d9448
wasuchok <wasuchokop@gmail.com>
2023-10-02 20:49:38 +0700

    Updated

--- main.cpp
+++ main.cpp
@@ -7,7 +7,43 @@ using namespace std;
 //function เมนู 1
 
 //function เมนู 2
+void restaurant_menu1() {
+    cout << "Press 1 Phat Kra Phrao Mu 50 Bath" << endl;
+    cout << "Press 2 Khao phat Khai 50 Bath" << endl;
+    cout << "Press 3 Phat Phong Kra Ri Thale 60 Bath" << endl;
+    cout << "Press 4 Khao Khaichiao Mu Sap 50 Bath" << endl;
+    cout << "Press 5 Suki Haeng Mu 50 Bath" << endl;
+    cout << "Press 6 Suki Nam Thale 60 Bath" << endl;
+    cout << "Press 7 Khao Plao 10 Bath" << endl;
+    cout << "Press 0 backward" << endl;
+}
+
+void restaurant_menu2() {
+    cout << "Press 1 Khaomankai Tom 50 Bath" << endl;
+    cout << "Press 2 Khaoman Kai Thot 60 Bath" << endl;
+    cout << "Press 3 Khao Kai Yang 55 Bath" << endl;
+    cout << "Press 4 Khao Mu Krop 70 Bath" << endl;
+    cout << "Press 5 Khao Khluk Kra Pi 50 Bath" << endl;
+    cout << "Press 6 Khao Mudaeng 55 Bath" << endl;
+    cout << "Press 0 backward" << endl;
+}
+
+void restaurant_menu3() {
+    cout << "Press 1 Kuaitiao Nam Sai" << endl;
+    cout << "Press 2 Kuaitiao Namtok" << endl;
+    cout << "Press 3 Kuaitiao Tomyam" << endl;
+    cout << "Press 4 Kuaitiao Yentafo" << endl;
+    cout << "Press 0 backward" << endl;
+}
+
+void drink_shop_menu4() {
+    cout << "Press 1 Nam Plao 10 Bath" << endl;
+    cout << "Press 2 Nam Pep Si 15 Bath" << endl;
+    cout << "Press 3 Namsom 15 Bath" << endl;
+    cout << "Press 4 Namkhaeng Plao 5 Bath" << endl;
+    cout << "Press 0 backward" << endl;
+}
 
 int main() {
- cout << "TEST TEST";
+ 
 }
