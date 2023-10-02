07f0c6211f6f889d8385ea36178c4e2de3b9ceb2
Adithep.T <jameza_love02@hotmail.com>
2023-10-02 21:09:24 +0700

    add void

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
 using namespace std;
 
 //function เมนูทั้งหมด
+void menu_list() {
 
+    cout << "Press 1 Ran Ahan Tam Sang Ti Noi " << endl;
+    cout << "Press 2 Ran Khaomankai " << endl;
+    cout << "Press 3 Ran Kuaitiao " << endl;
+    cout << "Press 4 Check bill " << endl;
+    cout << "Press 0 Close Program  " << endl;
+
+}
 //function เมนู 1
 
 //function เมนู 2
@@ -12,5 +19,7 @@ using namespace std;
 
 
 int main() {
- cout << "TEST TEST";
+
+
+
 }
