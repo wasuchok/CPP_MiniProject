722d2f52d11d4934a35763760f961d7f71c407e9
Adithep.T <jameza_love02@hotmail.com>
2023-10-02 21:12:59 +0700

    add_

--- main.cpp
+++ main.cpp
@@ -4,10 +4,9 @@ using namespace std;
 //function เมนูทั้งหมด
 void menu_list() {
 
-    cout << "Press 1 Ran Ahan Tam Sang Ti Noi " << endl;
-    cout << "Press 2 Ran Khaomankai " << endl;
-    cout << "Press 3 Ran Kuaitiao " << endl;
-    cout << "Press 4 Check bill " << endl;
+    cout << "Press 1 Restaurant " << endl;
+    cout << "Press 2 Food " << endl;
+    cout << "Press 3 Check bill " << endl;
     cout << "Press 0 Close Program  " << endl;
 
 }
