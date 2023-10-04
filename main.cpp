8a2348ea35564545fa60162ff2d36a1eb910a3ad
wasuchok <wasuchokop@gmail.com>
2023-10-04 19:29:38 +0700

    lastupdated

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void restaurant_menu1();
+
 bool check_status = false;
 int restaurant_menu, total = 0;
 
@@ -31,6 +33,237 @@ void restaurant_list() {
     : check_status = false;
 
     check_status = true;
+}
+
+void restaurant_menu2() {
+    cout << "Press 1 Khaoman kai" << endl;
+    cout << "Press 2 Khao Mudaeng 50 Bath" << endl;
+    cout << "Press 3 Khao MuKrop 55 Bath" << endl;
+    cout << "Press 4 Khao Khluk Kra Pi 50 Bath" << endl;
+    cout << "Press 5 Khao MudaengMuKrop 55 Bath" << endl;
+    cout << "Press 0 backward" << endl;
+
+    int restaurant_menu2;
+   
+    cout << "Please select food >> ", cin >> restaurant_menu2;
+
+    int restaurant_menu2_choice;
+    int restaurant_menu2_praphet;
+
+    if(restaurant_menu2 == 1){
+            cout << "Press 1 Khaoman Kai Tom  40 Bath" << endl;
+            cout << "Press 2 Khaoman Kai Yang 40 Bath" << endl;
+            cout << "Press 3 Khaoman Kai Thot 45 Bath" << endl;
+            cout << "Please select choice >> ", cin >> restaurant_menu2_choice;
+
+           
+
+                if(restaurant_menu2_choice == 1 || restaurant_menu2_choice == 2){
+                cout << "Press 1 Thammada" << endl;
+                cout << "Press 2 Phiset" << endl;
+                cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
+
+                        if(restaurant_menu2_praphet == 1){
+                            total += 40;
+                        }else if(restaurant_menu2_praphet == 2){
+                            total += 40 + 10;
+                        }
+                }
+
+                            else if(restaurant_menu2_choice == 3){
+                                cout << "Press 1 Thammada" << endl;
+                                cout << "Press 2 Phiset" << endl;
+                                cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
+
+                                if(restaurant_menu2_praphet == 1){
+                                    total += 45;
+                                }else if(restaurant_menu2_praphet == 2){
+                                    total += 45 + 10;
+                                }
+                            }
+
+
+        }else if(restaurant_menu2 == 2){
+            cout << "Press 1 Thammada" << endl;
+            cout << "Press 2 Phiset" << endl;
+            cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
+
+                if(restaurant_menu2_praphet == 1){
+                    total += 50;
+                }else if(restaurant_menu2_praphet == 2){
+                    total += 50 + 10;
+                }
+
+        }else if(restaurant_menu2 == 3){
+            cout << "Press 1 Thammada" << endl;
+            cout << "Press 2 Phiset" << endl;
+            cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
+
+                if(restaurant_menu2_praphet == 1){
+                    total += 55;
+                }else if(restaurant_menu2_praphet == 2){
+                    total += 55 + 10;
+                }
+        }else if(restaurant_menu2 == 4){
+            cout << "Press 1 Thammada" << endl;
+            cout << "Press 2 Phiset" << endl;
+            cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
+
+                if(restaurant_menu2_praphet == 1){
+                    total += 50;
+                }else if(restaurant_menu2_praphet == 2){
+                    total += 50 + 10;
+                }
+        }else if(restaurant_menu2 == 5){
+            cout << "Press 1 Thammada" << endl;
+            cout << "Press 2 Phiset" << endl;
+            cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
+
+                if(restaurant_menu2_praphet == 1){
+                    total += 55;
+                }else if(restaurant_menu2_praphet == 2){
+                    total += 55 + 10;
+                }
+        }
+
+}
+
+void additional_menu3_4() {
+    cout << "----- Special or Normal -----" << endl;
+    cout << "Press 1 Special" << endl;
+    cout << "Press 2 Normal" << endl;
+
+    int choice_additional_menu3;
+    cout << "Enter >> ", cin >> choice_additional_menu3;
+
+    if(choice_additional_menu3 == 1) {
+        total += 10;
+    } else if(choice_additional_menu3 == 2) {
+        total += 5;
+    } else {
+        cout << "If not selected, it will be set to Normal." << endl;
+        total += 5;
+    }
+}
+
+void additional_menu3_2() {
+    cout << "----- Choose meat -----" << endl;
+    cout << "Press 1 Mu Chin" << endl;
+    cout << "Press 2 Mu Sap" << endl;
+    cout << "Press 3 Nong Kai" << endl;
+    cout << "Press 4 Kai Chik" << endl;
+
+    int choice_additional_menu3;
+    cout << "Enter >> ", cin >> choice_additional_menu3;
+
+    if(choice_additional_menu3 == 1) {
+        total += 15;
+    } else if(choice_additional_menu3 == 2) {
+        total += 10;
+    } else if(choice_additional_menu3 == 3) {
+        total += 15;
+    } else if(choice_additional_menu3 == 4) {
+        total += 10;
+    }
+}
+
+void additional_menu3_1(int price) {
+    cout << "----- Choose noodles -----" << endl;
+    cout << "Press 1 Sen Lek" << endl;
+    cout << "Press 2 Sen Yai" << endl;
+    cout << "Press 3 Sen Ma Ma" << endl;
+    cout << "Press 4 Sen Mi" << endl;
+    cout << "Press 0 Backward" << endl;
+    total += price;
+
+    int choice_additional_menu3;
+    cout << "Enter >> ", cin >> choice_additional_menu3;
+    
+    if(choice_additional_menu3 == 1) {
+        total += 9;
+        additional_menu3_2();
+    } else if(choice_additional_menu3 == 2) {
+        total += 9;
+        additional_menu3_2();
+    } else if(choice_additional_menu3 == 3) {
+        total += 7;
+        additional_menu3_2();
+    } else if(choice_additional_menu3 == 4) {
+        total += 7;
+        additional_menu3_2();
+    } else if(choice_additional_menu3 == 0) {
+        total -= 40;
+    } else {
+        cout << "Invaild Choice" << endl;
+        total -= 40;
+    }
+}
+
+void restaurant_menu3() {
+    cout << "Press 1 Kuaitiao Nam Sai" << endl;
+    cout << "Press 2 Kuaitiao Namtok" << endl;
+    cout << "Press 3 Kuaitiao Tomyam" << endl;
+    cout << "Press 4 Kuaitiao Yentafo" << endl;
+    cout << "Press 0 backward" << endl;
+
+    int choice_restaurant_menu3;
+
+    cout << "Please select food >> ", cin >> choice_restaurant_menu3;
+
+    if(choice_restaurant_menu3 == 1) {
+        additional_menu3_1(30);
+    } else if(choice_restaurant_menu3 == 2) {
+        additional_menu3_1(35);
+    } else if(choice_restaurant_menu3 == 3) {
+        additional_menu3_1(40);
+    } else if(choice_restaurant_menu3 == 4) {
+        additional_menu3_1(40);
+    }
+}
+
+void drink_shop_menu4() {
+    cout << "Press 1 Nam Plao 10 Bath" << endl;
+    cout << "Press 2 Nam Pep Si 15 Bath" << endl;
+    cout << "Press 3 Namsom 15 Bath" << endl;
+    cout << "Press 4 Namkhaeng Plao 5 Bath" << endl;
+    cout << "Press 0 backward" << endl;
+}
+
+void menu_list() {
+    while (true) {
+    int choice_menu_list;
+
+    cout << "Press 1 Restaurant " << endl;
+    cout << "Press 2 Food " << endl;
+    cout << "Press 3 Check bill " << endl;
+    cout << "Total >> " << total << " Bath" << endl;
+    cout << "Press 0 Close Program  " << endl;
+
+    cout << "Please select menu >> ", cin >> choice_menu_list;
+
+    if(choice_menu_list == 0) {
+        break;
+    } else if(choice_menu_list == 1) {
+        restaurant_list();
+    } else if(choice_menu_list == 2) {
+        if(check_status) {
+            if(restaurant_menu == 1) {
+                restaurant_menu1();
+            } else if(restaurant_menu == 2) {
+                restaurant_menu2();
+            } else if(restaurant_menu == 3) {
+                restaurant_menu3();
+            }
+        } else {
+            alert_message("Please choose a restaurant first.");
+        }
+     }
+    }
+}
+
+
+int main() {
+    menu_list();
 }
 
 void restaurant_menu1() {
@@ -1636,230 +1869,4 @@ void restaurant_menu1() {
             }
         }
     }
-}
-
-
-void restaurant_menu2() {
-    cout << "Press 1 Khaoman kai" << endl;
-    cout << "Press 2 Khao Mudaeng 50 Bath" << endl;
-    cout << "Press 3 Khao MuKrop 55 Bath" << endl;
-    cout << "Press 4 Khao Khluk Kra Pi 50 Bath" << endl;
-    cout << "Press 5 Khao MudaengMuKrop 55 Bath" << endl;
-    cout << "Press 0 backward" << endl;
-
-    int restaurant_menu2;
-   
-    cout << "Please select food >> ", cin >> restaurant_menu2;
-
-    int restaurant_menu2_choice;
-    int restaurant_menu2_praphet;
-
-    if(restaurant_menu2 == 1){
-            cout << "Press 1 Khaoman Kai Tom  40 Bath" << endl;
-            cout << "Press 2 Khaoman Kai Yang 40 Bath" << endl;
-            cout << "Press 3 Khaoman Kai Thot 45 Bath" << endl;
-            cout << "Please select choice >> ", cin >> restaurant_menu2_choice;
-
-           
-
-                if(restaurant_menu2_choice == 1 || restaurant_menu2_choice == 2){
-                cout << "Press 1 Thammada" << endl;
-                cout << "Press 2 Phiset" << endl;
-                cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
-
-                        if(restaurant_menu2_praphet == 1){
-                            total += 40;
-                        }else if(restaurant_menu2_praphet == 2){
-                            total += 40 + 10;
-                        }
-                }
-
-                            else if(restaurant_menu2_choice == 3){
-                                cout << "Press 1 Thammada" << endl;
-                                cout << "Press 2 Phiset" << endl;
-                                cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
-
-                                if(restaurant_menu2_praphet == 1){
-                                    total += 45;
-                                }else if(restaurant_menu2_praphet == 2){
-                                    total += 45 + 10;
-                                }
-                            }
-
-
-        }else if(restaurant_menu2 == 2){
-            cout << "Press 1 Thammada" << endl;
-            cout << "Press 2 Phiset" << endl;
-            cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
-
-                if(restaurant_menu2_praphet == 1){
-                    total += 50;
-                }else if(restaurant_menu2_praphet == 2){
-                    total += 50 + 10;
-                }
-
-        }else if(restaurant_menu2 == 3){
-            cout << "Press 1 Thammada" << endl;
-            cout << "Press 2 Phiset" << endl;
-            cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
-
-                if(restaurant_menu2_praphet == 1){
-                    total += 55;
-                }else if(restaurant_menu2_praphet == 2){
-                    total += 55 + 10;
-                }
-        }else if(restaurant_menu2 == 4){
-            cout << "Press 1 Thammada" << endl;
-            cout << "Press 2 Phiset" << endl;
-            cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
-
-                if(restaurant_menu2_praphet == 1){
-                    total += 50;
-                }else if(restaurant_menu2_praphet == 2){
-                    total += 50 + 10;
-                }
-        }else if(restaurant_menu2 == 5){
-            cout << "Press 1 Thammada" << endl;
-            cout << "Press 2 Phiset" << endl;
-            cout << "Please select choice >> ", cin >> restaurant_menu2_praphet;
-
-                if(restaurant_menu2_praphet == 1){
-                    total += 55;
-                }else if(restaurant_menu2_praphet == 2){
-                    total += 55 + 10;
-                }
-        }
-
-}
-
-void additional_menu3_4() {
-    cout << "----- Special or Normal -----" << endl;
-    cout << "Press 1 Special" << endl;
-    cout << "Press 2 Normal" << endl;
-
-    int choice_additional_menu3;
-    cout << "Enter >> ", cin >> choice_additional_menu3;
-
-    if(choice_additional_menu3 == 1) {
-        total += 10;
-    } else if(choice_additional_menu3 == 2) {
-        total += 5;
-    } else {
-        cout << "If not selected, it will be set to Normal." << endl;
-        total += 5;
-    }
-}
-
-void additional_menu3_2() {
-    cout << "----- Choose meat -----" << endl;
-    cout << "Press 1 Mu Chin" << endl;
-    cout << "Press 2 Mu Sap" << endl;
-    cout << "Press 3 Nong Kai" << endl;
-    cout << "Press 4 Kai Chik" << endl;
-
-    int choice_additional_menu3;
-    cout << "Enter >> ", cin >> choice_additional_menu3;
-
-    if(choice_additional_menu3 == 1) {
-        total += 15;
-    } else if(choice_additional_menu3 == 2) {
-        total += 10;
-    } else if(choice_additional_menu3 == 3) {
-        total += 15;
-    } else if(choice_additional_menu3 == 4) {
-        total += 10;
-    }
-}
-
-void additional_menu3_1() {
-    cout << "----- Choose noodles -----" << endl;
-    cout << "Press 1 Sen Lek" << endl;
-    cout << "Press 2 Sen Yai" << endl;
-    cout << "Press 3 Sen Ma Ma" << endl;
-    cout << "Press 4 Sen Mi" << endl;
-    cout << "Press 0 Backward" << endl;
-    total += 40;
-
-    int choice_additional_menu3;
-    cout << "Enter >> ", cin >> choice_additional_menu3;
-    
-    if(choice_additional_menu3 == 1) {
-        total += 9;
-        additional_menu3_2();
-    } else if(choice_additional_menu3 == 2) {
-        total += 9;
-        additional_menu3_2();
-    } else if(choice_additional_menu3 == 3) {
-        total += 7;
-        additional_menu3_2();
-    } else if(choice_additional_menu3 == 4) {
-        total += 7;
-        additional_menu3_2();
-    } else if(choice_additional_menu3 == 0) {
-        total -= 40;
-    } else {
-        cout << "Invaild Choice" << endl;
-        total -= 40;
-    }
-}
-
-void restaurant_menu3() {
-    cout << "Press 1 Kuaitiao Nam Sai" << endl;
-    cout << "Press 2 Kuaitiao Namtok" << endl;
-    cout << "Press 3 Kuaitiao Tomyam" << endl;
-    cout << "Press 4 Kuaitiao Yentafo" << endl;
-    cout << "Press 0 backward" << endl;
-
-    int choice_restaurant_menu3;
-
-    cout << "Please select food >> ", cin >> choice_restaurant_menu3;
-
-    if(choice_restaurant_menu3 == 1) {
-        additional_menu3_1();
-    }
-}
-
-void drink_shop_menu4() {
-    cout << "Press 1 Nam Plao 10 Bath" << endl;
-    cout << "Press 2 Nam Pep Si 15 Bath" << endl;
-    cout << "Press 3 Namsom 15 Bath" << endl;
-    cout << "Press 4 Namkhaeng Plao 5 Bath" << endl;
-    cout << "Press 0 backward" << endl;
-}
-
-void menu_list() {
-    while (true) {
-    int choice_menu_list;
-
-    cout << "Press 1 Restaurant " << endl;
-    cout << "Press 2 Food " << endl;
-    cout << "Press 3 Check bill " << endl;
-    cout << "Total >> " << total << " Bath" << endl;
-    cout << "Press 0 Close Program  " << endl;
-
-    cout << "Please select menu >> ", cin >> choice_menu_list;
-
-    if(choice_menu_list == 0) {
-        break;
-    } else if(choice_menu_list == 1) {
-        restaurant_list();
-    } else if(choice_menu_list == 2) {
-        if(check_status) {
-            if(restaurant_menu == 1) {
-                restaurant_menu1();
-            } else if(restaurant_menu == 2) {
-                restaurant_menu2();
-            } else if(restaurant_menu == 3) {
-                restaurant_menu3();
-            }
-        } else {
-            alert_message("Please choose a restaurant first.");
-        }
-     }
-    }
-}
-
-
-int main() {
-    menu_list();
 }
